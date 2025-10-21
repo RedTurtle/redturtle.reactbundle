cb4bf9009d32e1b5a979437795c4b17655894623
Andrea Cecchi <andrea.cecchi85@gmail.com>
2025-10-21 17:57:38 +0200

    fix lint

--- setup.py
+++ setup.py
@@ -21,14 +21,13 @@ setup(
     # Get more from https://pypi.org/classifiers/
     classifiers=[
         "Environment :: Web Environment",
+        "Development Status :: 5 - Production/Stable",
         "Framework :: Plone",
         "Framework :: Plone :: Addon",
-        "Framework :: Plone :: 5.2",
-        "Framework :: Plone :: 6.0",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        "Framework :: Plone :: 6.1",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
@@ -48,7 +47,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.7",
+    python_requires=">=3.11",
     install_requires=[
         "setuptools",
     ],
