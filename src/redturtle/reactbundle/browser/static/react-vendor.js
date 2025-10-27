0e2c9e775e541eee551db08c88b869d84ec5fc9f
Giulia Ghisini <giulia.ghisini@redturtle.it>
2025-10-27 12:32:12 +0100

    react

--- src/redturtle/reactbundle/browser/static/react-vendor.js
+++ src/redturtle/reactbundle/browser/static/react-vendor.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
+
 
 window.React = React;
 window.ReactDOM = ReactDOM;
