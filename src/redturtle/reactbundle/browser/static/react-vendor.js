ab317e05ee8acfbafe1d0f9dbf8dfb01a00641cc
Giulia Ghisini <giulia.ghisini@redturtle.it>
2025-11-06 16:03:01 +0100

    added createPortal

--- src/redturtle/reactbundle/browser/static/react-vendor.js
+++ src/redturtle/reactbundle/browser/static/react-vendor.js
@@ -2,6 +2,14 @@ import React from 'react';
 //import ReactDOM from 'react-dom';
 import * as ReactDOM from 'react-dom/client';
 import * as ReactDOMServer from 'react-dom/server.browser';
+import * as ReactDOMLegacy from 'react-dom'; // <-- importa anche il modulo legacy
+
+// --- React 19 createPortal shim ---
+if (!ReactDOM.createPortal && ReactDOMLegacy.createPortal) {
+  // aggiungiamo la vecchia API al namespace client
+  ReactDOM.createPortal = ReactDOMLegacy.createPortal;
+}
+// --- end shim ---
 
 window.React = React;
 window.ReactDOM = ReactDOM;
