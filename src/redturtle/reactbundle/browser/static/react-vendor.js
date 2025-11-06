9478b4efb8893dec05d32e41dee977a9291f9a41
Giulia Ghisini <giulia.ghisini@redturtle.it>
2025-11-06 14:54:12 +0100

    feat: added reactDOMServer

--- src/redturtle/reactbundle/browser/static/react-vendor.js
+++ src/redturtle/reactbundle/browser/static/react-vendor.js
@@ -1,7 +1,8 @@
 import React from 'react';
 //import ReactDOM from 'react-dom';
 import * as ReactDOM from 'react-dom/client';
-
+import * as ReactDOMServer from 'react-dom/server.browser';
 
 window.React = React;
 window.ReactDOM = ReactDOM;
+window.ReactDOMServer = ReactDOMServer;
